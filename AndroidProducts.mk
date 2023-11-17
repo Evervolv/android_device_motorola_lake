0bcd8128a5c8d8bbfcb6dcfacd0a46ae7b9d05d2
William Bellavance Jr <flintman@flintmancomputer.com>
2023-11-17 08:21:34 -0500

    lake: convert to Evervolv
    Change-Id: I35b78135d8ff60fb297139aec6f8f22b09e0610d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lake.mk
+    $(LOCAL_DIR)/ev_lake.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lake-eng \
-    lineage_lake-user \
-    lineage_lake-userdebug
+    ev_lake-eng \
+    ev_lake-user \
+    ev_lake-userdebug
--- ev_lake.mk
+++ ev_lake.mk
@@ -1,6 +1,6 @@
-# Inherit some common Lineage stuff.
+# Inherit some common Evervolv stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -12,7 +12,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := lake
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7) plus
-PRODUCT_NAME := lineage_lake
+PRODUCT_NAME := ev_lake
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lake \
